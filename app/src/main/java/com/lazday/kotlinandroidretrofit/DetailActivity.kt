5cb48b5ce04285a80850b145380df1e149a5c48b
mchirsydd <mchirsyd@yahoo.co.id>
2020-10-17 05:01:58 +0700

    update adapter

--- app/src/main/java/com/lazday/kotlinandroidretrofit/DetailActivity.kt
+++ app/src/main/java/com/lazday/kotlinandroidretrofit/DetailActivity.kt
@@ -2,14 +2,15 @@ package com.lazday.kotlinandroidretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class DetailActivity : AppCompatActivity() {
-
-    private val intentTitle = intent.getStringExtra("intent_title")
-    private val intentImage = intent.getStringExtra("intent_title")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        val intentTitle = intent.getStringExtra("intent_title")
+        val intentImage = intent.getStringExtra("intent_image")
+        Log.d("DetailActivity", intentTitle)
     }
 }
--- app/src/main/java/com/lazday/kotlinandroidretrofit/MainAdapter.kt
+++ app/src/main/java/com/lazday/kotlinandroidretrofit/MainAdapter.kt
@@ -1,10 +1,10 @@
 package com.lazday.kotlinandroidretrofit
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.adapter_main.view.*
 import kotlin.collections.ArrayList
 
@@ -21,6 +21,10 @@ class MainAdapter (var photos: ArrayList<MainModel>, val listener: OnAdapterList
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val photo = photos[position]
         holder.view.textView.text = photo.title
+        Glide.with(holder.view)
+            .load(photo.url)
+            .placeholder(R.drawable.img_placeholder)
+            .into(holder.view.imageView)
         holder.view.setOnClickListener { listener.onClick( photo ) }
     }
 
